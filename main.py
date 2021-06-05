bd11fa30f1fe6bdac84e5824e99bb6baf59125fe
caodongliang <cao.dongliang97@gmail.com>
2021-06-05 15:22:58 +0800

    small fix

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ if __name__=='__main__':
         # start training
         train(model, train_dataloader, max_epoch, device)
     elif mode == 'test':
+        # TODO
         pass
     else:
         raise ValueError(f'Unknown mode: {mode}')
