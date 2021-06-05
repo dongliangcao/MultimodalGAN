14d081765410c402252635dcad78a3a5b47930ed
caodongliang <cao.dongliang97@gmail.com>
2021-06-05 11:05:34 +0800

    model and dataset init

---
+++ datasets/dataset.py
@@ -0,0 +1,86 @@
+import os
+
+import numpy as np
+
+from glob import glob
+
+import torch
+import torch.utils.data as data
+
+import imageio
+
+def read_image(filename):
+    return imageio.imread(filename).astype(np.float32) / np.float32(255.0)
+
+def read_mask(filename):
+    return imageio.imread(filename)
+
+def numpy2torch(array):
+    assert(isinstance(array, np.ndarray))
+    if array.ndim == 3:
+        array = np.transpose(array, (2, 0, 1))
+    else:
+        array = np.expand_dims(array, axis=0)
+    return torch.from_numpy(array.copy()).float()
+
+class MRDataset(data.Dataset):
+    """
+    Dataset to read MR data in .png
+    source data: T1, T2, target data: FLAIR, DIR 
+    """
+    def __init__(self, root, dtype='train'):
+        if not os.path.isdir(root):
+            raise ValueError(f'f{root} is not found')
+        
+        if dtype == 'train':
+            self.T1_filenames = sorted(glob(os.path.join(root, 'trainT1', '*.png')))
+            self.T2_filenames = sorted(glob(os.path.join(root, 'trainT2', '*.png')))
+            self.FLAIR_filenames = sorted(glob(os.path.join(root, 'trainS1', '*.png')))
+            self.DIR_filenames = sorted(glob(os.path.join(root, 'trainS2', '*.png')))
+            self.mask_filenames = sorted(glob(os.path.join(root, 'train_mask', '*.png')))
+        elif dtype == 'test':
+            self.T1_filenames = sorted(glob(os.path.join(root, 'testT1', '*.png')))
+            self.T2_filenames = sorted(glob(os.path.join(root, 'testT2', '*.png')))
+            self.FLAIR_filenames = sorted(glob(os.path.join(root, 'testS1', '*.png')))
+            self.DIR_filenames = sorted(glob(os.path.join(root, 'testS2', '*.png')))
+            self.mask_filenames = sorted(glob(os.path.join(root, 'test_mask', '*.png')))
+        else:
+            raise ValueError(f'Unknwon dtype: {dtype}')
+
+        # sanity check
+        assert len(self.T1_filenames) != 0 and len(self.T1_filenames) == len(self.T2_filenames)
+        assert len(self.T1_filenames) == len(self.FLAIR_filenames) and len(self.T1_filenames) == len(self.FLAIR_filenames)
+        assert len(self.T1_filenames) == len(self.DIR_filenames) and len(self.T1_filenames) == len(self.mask_filenames)
+
+        self._size = len(self.T1_filenames)
+
+    def __len__(self):
+        return self._size
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return [self[ii] for ii in range(*index.indices(len(self)))]
+        else:
+            T1 = read_image(self.T1_filenames[index])
+            T2 = read_image(self.T2_filenames[index])
+            FLAIR = read_image(self.FLAIR_filenames[index])
+            DIR = read_image(self.DIR_filenames[index])
+            mask = read_mask(self.mask_filenames[index])
+
+            T1, T2, FLAIR, DIR, mask = numpy2torch(T1), numpy2torch(T2), numpy2torch(FLAIR), numpy2torch(DIR), numpy2torch(mask)
+            return {
+                'T1': T1,
+                'T2': T2,
+                'FLAIR': FLAIR,
+                'DIR': DIR,
+                'mask': mask
+            }
+
+class MRTrainDataset(MRDataset):
+    def __init__(self, root):
+        super().__init__(root, dtype='train')
+
+class MRTestDataset(MRDataset):
+    def __init__(self, root):
+        super().__init__(root, dtype='test')
+    
---
+++ network/CycleGAN.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def convleakyrelu(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation=1, groups=1, bias=True, normalization=True):
+    if not normalization:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias=bias), 
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias=False), 
+            nn.InstanceNorm2d(out_channels, affine=True, track_running_stats=True),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+
+def convrelu(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation=1, groups=1, bias=True, normalization=True):
+    if not normalization:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias=bias), 
+            nn.ReLU(inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias=False), 
+            nn.InstanceNorm2d(out_channels, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True)
+        )
+
+def deconvrelu(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation=1, groups=1, bias=True, normalization=True):
+    if not normalization:
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=padding, groups=groups, bias=bias, dilation=dilation),
+            nn.ReLU(inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=padding, groups=groups, bias=False, dilation=dilation),
+            nn.InstanceNorm2d(out_channels, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True)
+        )
+
+class CycleGAN(nn.Module):
+    def __init__(self, in_channels=2):
+        super().__init__()
+        self.in_channels = in_channels
+        # discriminator 
+        self.D_A = Discriminator(in_channels)
+        self.D_B = Discriminator(in_channels)
+        # generator
+        self.G_A2B = Generator(in_channels)
+        self.G_B2A = Generator(in_channels)
+
+    def forward(self, real_A, real_B, mask):
+        synthetic_B, attn_synthetic_B = self.G_A2B(real_A, mask)
+        synthetic_A, attn_synthetic_A = self.G_B2A(real_B, mask)
+        # Do not uptda ediscriminator weights during generator training
+        with torch.no_grad():
+            dA_guess_synthetic = self.D_A(synthetic_A)
+            dB_guess_synthetic = self.D_B(synthetic_B)
+        reconstructed_B, attn_rec_B = self.G_A2B(synthetic_A, mask)
+        reconstructed_A, attn_rec_A = self.G_B2A(synthetic_B, mask)
+        return reconstructed_A, reconstructed_B, attn_rec_B, dA_guess_synthetic, dB_guess_synthetic
+          
+
+class Generator(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        # layer 1
+        self.layer1 = nn.Sequential(
+            nn.ReflectionPad2d(padding=3),
+            nn.Conv2d(2, 48, kernel_size=7, stride=1),
+            nn.InstanceNorm2d(48, affine=True, track_running_stats=True),
+            nn.ReLU(inplace=True)
+        )
+        # layer 2
+        self.layer2 = convrelu(48, 72)
+        # layer 3
+        self.layer3 = convrelu(72, 128)
+        # lyaer 4-12: residual layer
+        layers = []
+        for _ in range(4, 13):
+            layers.append(ResidualBlock(128))
+        self.residual_blocks = nn.Sequential(*layers)
+        # layer 13
+        self.layer13 = deconvrelu(128, 72)
+        # layer 14
+        self.layer14 = nn.Sequential(
+            deconvrelu(72, 48),
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(48, in_channels, kernel_size=7),
+            nn.Tanh()
+        )
+
+    def forward(self, img, mask):
+        out = self.layer3(self.layer2(self.layer1(img)))
+        out = self.residual_blocks(out)
+        out = self.layer14(self.layer13(out))
+        out_attn = out * mask
+        return out, out_attn
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        # layer 1 (instance normalization is not used for this layer)
+        self.layer1 = convleakyrelu(in_channels, 64, kernel_size=4, stride=2, padding=1, normalization=False)
+        # layer 2
+        self.layer2 = convleakyrelu(64, 128, kernel_size=4, stride=2, padding=1)
+        # layer 3
+        self.layer3 = convleakyrelu(128, 256, kernel_size=4, stride=2, padding=1)
+        # layer 4
+        self.layer4 = convleakyrelu(256, 512, kernel_size=4, stride=2, padding=1)
+        # output layer
+        self.out = nn.Sequential(
+            nn.Conv2d(512, 1, kernel_size=4, padding=2),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img):
+        return self.out(self.layer4(self.layer3(self.layer2(self.layer1(img)))))
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels, channels, 3, 1, 1)
+        self.norm1 = nn.InstanceNorm2d(channels, affine=True, track_running_stats=True)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(channels, channels, 3, 1, 1)
+        self.norm2 = nn.InstanceNorm2d(channels, affine=True, track_running_stats=True)
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        y = self.relu1(self.norm1(self.conv1(x)))
+        y = self.relu2(self.norm2(self.conv2(y)))
+        return x + y
+
+if __name__=='__main__':
+    from torchsummary import summary
+    GAN = CycleGAN(2).cuda()
+    summary(GAN, input_data=((2, 100, 100), (2, 100, 100), (1, 100, 100)))
